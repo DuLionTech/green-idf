20214aa1a77cdad6751330766d27c6245f1e4525
Phillip DuLion <phillip@dulion.com>
2025-09-25 14:25:05 -0700

    Add simple cargo run and few refactorings

--- src/relay.rs
+++ src/relay.rs
@@ -1,9 +1,7 @@
-use esp_idf_svc::hal::delay::FreeRtos;
 use crate::utils::Result;
+use esp_idf_svc::hal::delay::FreeRtos;
 use esp_idf_svc::hal::gpio::Output;
 use esp_idf_svc::hal::gpio::*;
-use crate::relay::RelayPin::{Ch1, Ch2, Ch3, Ch4, Ch5, Ch6};
-use crate::relay::RelayState::{Close, Open};
 
 pub enum RelayPin {
     Ch1,
@@ -46,34 +44,34 @@ impl<'a> Relay<'a> {
             ch6,
         }
     }
-    
+
     pub fn switch(&mut self, relay: RelayPin, state: RelayState) -> Result<()> {
+        use RelayPin::*;
+        use RelayState::*;
         match state {
-            RelayState::Close => {
-                match relay {
-                    RelayPin::Ch1 => self.ch1.set_high()?,
-                    RelayPin::Ch2 => self.ch2.set_high()?,
-                    RelayPin::Ch3 => self.ch3.set_high()?,
-                    RelayPin::Ch4 => self.ch4.set_high()?,
-                    RelayPin::Ch5 => self.ch5.set_high()?,
-                    RelayPin::Ch6 => self.ch6.set_high()?,
-                }
+            Close => match relay {
+                Ch1 => self.ch1.set_high()?,
+                Ch2 => self.ch2.set_high()?,
+                Ch3 => self.ch3.set_high()?,
+                Ch4 => self.ch4.set_high()?,
+                Ch5 => self.ch5.set_high()?,
+                Ch6 => self.ch6.set_high()?,
             },
-            RelayState::Open => {
-                match relay {
-                    RelayPin::Ch1 => self.ch1.set_low()?,
-                    RelayPin::Ch2 => self.ch2.set_low()?,
-                    RelayPin::Ch3 => self.ch3.set_low()?,
-                    RelayPin::Ch4 => self.ch4.set_low()?,
-                    RelayPin::Ch5 => self.ch5.set_low()?,
-                    RelayPin::Ch6 => self.ch6.set_low()?,
-                }
+            Open => match relay {
+                Ch1 => self.ch1.set_low()?,
+                Ch2 => self.ch2.set_low()?,
+                Ch3 => self.ch3.set_low()?,
+                Ch4 => self.ch4.set_low()?,
+                Ch5 => self.ch5.set_low()?,
+                Ch6 => self.ch6.set_low()?,
             },
         };
         Ok(())
     }
-    
+
     pub fn sequence(&mut self) -> Result<()> {
+        use RelayPin::*;
+        use RelayState::*;
         self.switch(Ch1, Close)?;
         FreeRtos::delay_ms(500);
         self.switch(Ch2, Close)?;
