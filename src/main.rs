4a7dd93d3e67697a6c70917be82bca29407ff512
Phillip DuLion <phillip@dulion.com>
2025-09-25 16:09:47 -0700

    Add custom DHCP hostname

--- src/main.rs
+++ src/main.rs
@@ -16,8 +16,11 @@ use esp_idf_svc::ipv4::{
 use esp_idf_svc::netif::{EspNetif, NetifConfiguration, NetifStack};
 use esp_idf_svc::nvs::EspDefaultNvsPartition;
 use esp_idf_svc::wifi::{
-    AuthMethod, BlockingWifi, ClientConfiguration, Configuration as WifiConfiguration, EspWifi, WifiDriver,
+    AuthMethod, BlockingWifi, ClientConfiguration, Configuration as WifiConfiguration, EspWifi,
+    WifiDriver,
 };
+
+const HOSTNAME: &str = "green";
 const WIFI_SSID: &str = env!("ESP_WIFI_SSID");
 const WIFI_PASS: &str = env!("ESP_WIFI_PASS");
 const STACK_SIZE: usize = 10_240;
@@ -53,18 +56,16 @@ fn main() -> Result<()> {
     }
 }
 
-fn configure_wifi<'a>(wifi: WifiDriver) -> Result<EspWifi> {
-    let dhcp_config = IpClientConfiguration::DHCP(
-        DHCPClientSettings {
-            hostname: Some(to_string("green")?),
-        },
-    );
+fn configure_wifi<'a>(driver: WifiDriver) -> Result<EspWifi> {
+    let dhcp_config = IpClientConfiguration::DHCP(DHCPClientSettings {
+        hostname: Some(to_string(HOSTNAME)?),
+    });
     let netif_config = NetifConfiguration {
         ip_configuration: Some(IpConfiguration::Client(dhcp_config)),
         ..NetifConfiguration::wifi_default_client()
     };
     let mut wifi = EspWifi::wrap_all(
-        wifi,
+        driver,
         EspNetif::new_with_conf(&netif_config)?,
         #[cfg(esp_idf_esp_wifi_softap_support)]
         EspNetif::new(NetifStack::Ap)?,
