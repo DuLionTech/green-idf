b8ec688677c1e7dad842800acb342c7028717316
Phillip DuLion <phillip@dulion.com>
2025-10-01 17:59:05 -0700

    MQTT refactor for readability

--- src/mqtt.rs
+++ src/mqtt.rs
@@ -1,5 +1,7 @@
 use crate::utils::Result;
-use esp_idf_svc::mqtt::client::{EspMqttClient, EventPayload, MqttClientConfiguration, QoS};
+use esp_idf_svc::mqtt::client::{
+    EspMqttClient, EspMqttConnection, EventPayload, MqttClientConfiguration, QoS,
+};
 use log::info;
 use std::time::Duration;
 
@@ -23,34 +25,11 @@ impl<'d> Mqtt<'d> {
     }
 
     pub fn start(&mut self) -> Result<()> {
-        let (mut client, mut connection) = EspMqttClient::new(MQTT_URL, &self.conf)?;
+        let (client, connection) = EspMqttClient::new(MQTT_URL, &self.conf)?;
         info!("MQTT listener starting");
         if let Err(e) = std::thread::Builder::new()
             .stack_size(1024 * 6)
-            .spawn(move || {
-                info!("MQTT listener running");
-                while let Ok(event) = connection.next() {
-                    match event.payload() {
-                        EventPayload::Received {
-                            topic,
-                            data,
-                            details,
-                            ..
-                        } => {
-                            info!(
-                                "MQTT listener received on topic {:?} with QoS {:?}: {}",
-                                topic,
-                                details,
-                                str::from_utf8(data).unwrap_or("Invalid UTF-8")
-                            );
-                        }
-                        _ => {
-                            info!("MQTT listener event: {:?}", event.payload());
-                        }
-                    }
-                }
-                info!("MQTT listener stopped");
-            })
+            .spawn(|| listen(connection))
         {
             info!("MQTT listener error: {:?}", e);
         };
@@ -58,27 +37,52 @@ impl<'d> Mqtt<'d> {
         info!("MQTT publisher starting");
         if let Err(e) = std::thread::Builder::new()
             .stack_size(1024 * 6)
-            .spawn(move || {
-                info!("MQTT publisher running");
-                while let Err(e) = client.subscribe(MQTT_TOPIC, QoS::AtMostOnce) {
-                    info!("MQTT publisher subscribe error: {:?}", e);
-                    std::thread::sleep(Duration::from_millis(500));
-                }
-                info!("MQTT publisher subscribed");
-                std::thread::sleep(Duration::from_millis(500));
-                loop {
-                    if let Err(e) =
-                        client.enqueue(MQTT_TOPIC, QoS::AtMostOnce, false, MQTT_PAYLOAD.as_ref())
-                    {
-                        info!("MQTT publisher enqueue error: {:?}", e);
-                        break;
-                    }
-                    std::thread::sleep(Duration::from_secs(2));
-                }
-            })
+            .spawn(move || publish(client))
         {
             info!("MQTT publisher error: {:?}", e);
         }
         Ok(())
+    }
+}
+
+fn listen(mut connection: EspMqttConnection) {
+    info!("MQTT listener running");
+    while let Ok(event) = connection.next() {
+        match event.payload() {
+            EventPayload::Received {
+                topic,
+                data,
+                details,
+                ..
+            } => {
+                info!(
+                    "MQTT listener received on topic {:?} with QoS {:?}: {}",
+                    topic,
+                    details,
+                    str::from_utf8(data).unwrap_or("Invalid UTF-8")
+                );
+            }
+            _ => {
+                info!("MQTT listener event: {:?}", event.payload());
+            }
+        }
+    }
+    info!("MQTT listener stopped");
+}
+
+fn publish(mut client: EspMqttClient) {
+    info!("MQTT publisher running");
+    while let Err(e) = client.subscribe(MQTT_TOPIC, QoS::AtMostOnce) {
+        info!("MQTT publisher subscribe error: {:?}", e);
+        std::thread::sleep(Duration::from_millis(500));
+    }
+    info!("MQTT publisher subscribed");
+    std::thread::sleep(Duration::from_millis(500));
+    loop {
+        if let Err(e) = client.enqueue(MQTT_TOPIC, QoS::AtMostOnce, false, MQTT_PAYLOAD.as_ref()) {
+            info!("MQTT publisher enqueue error: {:?}", e);
+            break;
+        }
+        std::thread::sleep(Duration::from_secs(2));
     }
 }
